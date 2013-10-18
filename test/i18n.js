5859033ff3851d79b686f581e8c2da2172e43678
Ivan Koretskiy <gillbeits@gmail.com>
2013-10-18 16:11:38 +0400

    new

--- test/i18n.js
+++ test/i18n.js
@@ -1,4 +1,28 @@
 var i18n = require('../');
-//i18n.mongoInit();
+var mongoose = require('mongoose');
 
-console.log(i18n['node-i18n'].schema.paths);
+//var db = mongoose.connection;
+//mongoose.connect("mongodb://localhost:27017/test");
+//
+//
+//
+//db.once('open', function callback () {
+//	mongoose.models.i18nMain
+//		.findOne({string: "Какой-то текст", locale: "ru_RU"})
+//		.populate({path: 'r_string', select: 'string', match: {locale: 'en_US'}})
+//		.exec(function(err, docs){
+//			console.log(docs);
+//		})
+//	;
+//});
+
+
+i18n.configInit( { locale: "ko_KR" } );
+i18n.mongoInit();
+i18n.add("Какой-то текст", "누군가가 텍스트");
+i18n.i18nGetLocale(function(){
+	console.log( i18n.__dict );
+});
+
+
+
