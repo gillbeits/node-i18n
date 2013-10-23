94233cb7e8f6ec2db20bafa35d3a2a458735e3dd
Ivan Koretskiy <gillbeits@gmail.com>
2013-10-23 18:57:06 +0400

    small fixes

--- test/i18n.js
+++ test/i18n.js
@@ -8,7 +8,7 @@ i18n.mongoInit();
 Sync(function(){
 
 	i18n.add.sync(null, {
-		"Привет %s": "Hello %s"
+		"Привет, %s": "Hello, %s"
 	}, 'en_US');
 
 	i18n.i18nGetLocale.sync();
@@ -18,8 +18,9 @@ Sync(function(){
 	console.log( i18n.__dict );
 
 	var user = "gillbeits";
-	console.log( fmt.vsprintf("Привет %s", [user]) );
-	console.log( i18n.__("Привет %s", [user]) );
+	console.log( fmt.vsprintf("Привет, %s", [user]) );
+	console.log( i18n.__("Привет, %s", [user]) );
+	console.log( i18n.__("Как поживаете, %s?", [user]) );
 
 	console.log('### end print');
 });
