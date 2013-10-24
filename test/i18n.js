c8ca360766ea035c717febfa27a9e15cdf46f702
Ivan Koretskiy <gillbeits@gmail.com>
2013-10-24 23:44:52 +0400

    some fixes

--- test/i18n.js
+++ test/i18n.js
@@ -2,15 +2,26 @@ var i18n = require('../')
 	, fmt = require('sprintf-js')
 	, Fiber = require('fibers');
 
-i18n.configInit( { locale: "de_DE" } );
+i18n.configInit( { defaultLocale: 'en_US', locale: "de_DE" } );
 
 i18n.Fiber(function(){
 	i18n.add({
 		"Welcome to my World, %s": "Willkommen in meiner Welt, %s"
 	}, 'de_DE');
 
+	i18n.add({
+		"Welcome to my World, %s": "Добро пожаловать в мой мир, %s"
+	}, 'ru_RU');
+
 	i18n.i18nGetLocale();
 
-	console.log( i18n.__dict );
+	console.log( __('Welcome to my World, %s') );
 	console.log( __('Welcome to my World, %s', ["Mike"]) );
+	console.log( __('Welcome to my World, %s', 'core', 'ru_RU', ["Mike"]) );
+
+	console.log( i18n.__dict );
 }).run();
+
+//i18n.i18nGetString('Welcome to my World, %s', function(err, doc){
+//	console.log(doc);
+//});
